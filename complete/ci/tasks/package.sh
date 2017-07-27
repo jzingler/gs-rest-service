9d0ffcbb3b27990864e94327509fe65096f3bf27
Jake Zingler <jzingler@pivotal.io>
2017-07-27 16:39:32 -0700

    update pipeline

--- complete/ci/tasks/package.sh
+++ complete/ci/tasks/package.sh
@@ -6,5 +6,5 @@ pushd source-code
 	mvn clean package
 popd
 
-cp source-code/manifest.yml  build-output/.
-cp source-code/target/gs-rest-service-concourse-0.1.0.jar  build-output/.
+cp source-code/complete/manifest.yml  build-output/.
+cp source-code/complete/target/gs-rest-service-concourse-0.1.0.jar  build-output/.
