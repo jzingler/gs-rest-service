c8120a8b2be6d5b1f2fdb95b96f1cbd79c242cbf
Jake Zingler <jzingler@pivotal.io>
2017-07-28 14:43:26 -0700

    update pipe

--- complete/ci/tasks/package.sh
+++ complete/ci/tasks/package.sh
@@ -5,7 +5,7 @@ set -e -x
 pushd source-code/complete
 	mvn -Dmaven.repo.local=.m2 clean package
 	
-	cp manifest.yml  build-output/.
-	cp target/gs-rest-service-0.1.0.jar  build-output/.
+	cp manifest.yml  ../../build-output/.
+	cp target/gs-rest-service-0.1.0.jar  ../../build-output/.
 popd
 
